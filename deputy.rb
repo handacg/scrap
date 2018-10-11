e66f15ee779f4cae772e716ec2bcb4b8419afbc9
handacg <handa.cheng@gmail.com>
2018-10-11 22:12:13 +0200

    added depute

--- deputy.rb
+++ deputy.rb
@@ -1,24 +1,38 @@
-require 'nokogiri'   
+require 'pry'
+require 'nokogiri'
 require 'open-uri'
 
-
-def email
-	page = Nokogiri::HTML(open("https://www.nosdeputes.fr/damien-abad"))
-	return page.xpath("/html/body/div[1]/div[5]/div/div[2]/div[1]/ul[2]/li[1]/ul/li[1]/a").text
+def get_emails(urls)
+  final_array = []
+  i = 0
+  urls.each do |url|
+    hash = {}
+    webpage = Nokogiri::HTML(open("https://www.nosdeputes.fr#{url}"))
+    hash['first_name'] = webpage.xpath('//*[@id="corps_page"]/div/div[1]/div[1]/h1').text.split(" ")[0]
+    hash['last_name'] = webpage.xpath('//*[@id="corps_page"]/div/div[1]/div[1]/h1').text.split(" ")[1]
+    begin
+      hash['email'] = webpage.xpath('//*[@id="b1"]/ul[2]/li[1]/ul/li/a')[0].text
+    rescue
+      hash['email'] = "Non renseigné"
+    end
+    final_array << hash
+    print "\rScrapping en cours: #{i} / #{urls.length}"
+    i += 1
+  end
+  final_array
 end
 
-
-def deputy
-	array =[]
-	page = Nokogiri::HTML(open("https://www.nosdeputes.fr/depute"))
-	page.css("td>a").each do |x|
-		hash[x] = email("https://www.nosdeputes.fr/#{x['href']}")
- end
-  deputy
+def get_deputies
+  array = []
+  webpage = Nokogiri::HTML(open("https://www.nosdeputes.fr/deputes"))
+  webpage.css('td>a').each do |inner|
+    array << inner['href']
+  end
+  puts get_emails(array)
 end
 
-puts deputy
+def perform
+  get_deputies
+end
 
-
-
-
+perform
